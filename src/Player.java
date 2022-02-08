d47edbca43fd4d98a82fc27a18781f631f8d4cf7
hvpg23 <96022942+hvpg23@users.noreply.github.com>
2022-02-08 13:49:45 -0500

    Added the content that was provided to us

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,17 @@
-public class Player {
+public enum Player {
+    BLACK, WHITE;
+
+    /**
+     * Return the {@code Player} whose turn is next.
+     *
+     * @return the {@code Player} whose turn is next
+     */
+    public Player next() {
+        if (this == BLACK)
+            return WHITE;
+        else
+            return BLACK;
+
+        //	return this == BLACK ? WHITE : BLACK; ternary condition that could be used instead
+    }
 }
