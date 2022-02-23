24f117c446e43980c83afb30086173ec17f0f748
hvpg23 <96022942+hvpg23@users.noreply.github.com>
2022-02-23 00:09:40 -0500

    Played with the rook just a little bit. I commented out the code I wrote because it was wrong, but left it so you could see my line of thinking.

--- src/Chess/Rook.java
+++ src/Chess/Rook.java
@@ -15,7 +15,24 @@ public class Rook extends ChessPiece {
 		boolean valid = true;
 
 
-        // More code is needed
+//		// if the rook doesn't within the row return false
+//		if (board[move.toRow][move.toColumn] != board[move.toRow][move.fromColumn]) {
+//			System.out.println("Only move within the row");
+//			valid = false;
+//		}
+//
+//		// if the rook doesn't within the column return false
+//		if (board[move.toRow][move.toColumn] != board[move.fromColumn][move.toColumn]) {
+//			System.out.println("Only move within the column");
+//			valid = false;
+//		}
+
+		if (board[move.toRow][move.toColumn] != null) {
+			System.out.println("Can't move there");
+			valid = false;
+		}
+
+
         return valid;
 		
 	}
