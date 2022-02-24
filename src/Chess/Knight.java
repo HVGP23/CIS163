92f8fbab04d527954a8f9cfbc2669fb0620cd78a
hvpg23 <96022942+hvpg23@users.noreply.github.com>
2022-02-24 17:31:18 -0500

    Modified the Knight Class

--- src/Chess/Knight.java
+++ src/Chess/Knight.java
@@ -1,5 +1,7 @@
 package Chess;
 
+import static java.lang.Math.abs;
+
 public class Knight extends ChessPiece {
 
 	public Knight(Player player) {
@@ -12,10 +14,46 @@ public class Knight extends ChessPiece {
 
 	public boolean isValidMove(Move move, IChessPiece[][] board){
 
+		// difference between the rows
+		int rowDiff0 = abs(move.fromRow - move.toRow);
 
+		// difference between the columns
+		int colDiff0 = abs(move.fromColumn - move.toColumn);
 
-		return true;
-		
+		// Up two over one OR down two over one
+		if (rowDiff0 == 2 && colDiff0 == 1) {
+			return true;
+		}
+
+		// up one over two OR down one over two
+		if (rowDiff0 == 1 && colDiff0 == 2) {
+			return true;
+		}
+
+		if (board[move.toRow][move.toColumn] == null) {
+			return true;
+		}
+
+		// if there is a chess piece where we are moving to
+		if (board[move.toRow][move.toColumn] != null) {
+
+			// allows white rook to take only black chess pieces
+			if (board[move.fromRow][move.fromColumn].player() == Player.WHITE &&
+					board[move.toRow][move.toColumn].player() == Player.BLACK) {
+				System.out.println("You took black chess piece");		// DELETE
+				return true;
+			}
+
+			// allows black rook to take only white chess pieces
+			 if ((board[move.fromRow][move.fromColumn].player() == Player.BLACK) &&
+					(board[move.toRow][move.toColumn].player() == Player.WHITE)) {
+				 System.out.println("You took white chess piece");        // DELETE
+				 return true;
+			 }
+		}
+
+		System.out.println(move);	// DELETE
+
+		return false;
 	}
-
 }
